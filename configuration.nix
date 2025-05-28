8f19c568803e76afd3437e258ccc31c6bcd99f2a
SiddharthPant <spsiddharthpant@gmail.com>
2025-05-28 11:46:53 +0530

    Chrome enable hw accel and wayland support

--- configuration.nix
+++ configuration.nix
@@ -159,7 +159,16 @@
     systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
-      google-chrome
+      (google-chrome.override {
+        commandLineArgs = [
+	  "--enable-features=AcceleratedVideoEncoder,VaapiOnNvidiaGPUs,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
+          "--enable-features=VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport"
+          "--enable-features=UseMultiPlaneFormatForHardwareVideo"
+          "--ignore-gpu-blocklist"
+          "--enable-zero-copy"
+	  "--ozone-platform=wayland"
+	];
+      })
       neovim
       git
       eza
