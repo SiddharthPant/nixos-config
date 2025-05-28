6b9a279c61463720dfe1aab0cc0a97aadd271017
SiddharthPant <spsiddharthpant@gmail.com>
2025-05-28 12:02:08 +0530

    Make generations last for a month

--- configuration.nix
+++ configuration.nix
@@ -219,7 +219,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 1w";
+    options = "--delete-older-than 1m";
   };
 
   # This value determines the NixOS release from which the default
