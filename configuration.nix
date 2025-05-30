f6a1f7e9e88786362252f664845ecf1c40a4aa8c
SiddharthPant <spsiddharthpant@gmail.com>
2025-05-30 11:09:49 +0530

    fix openrazer config

--- configuration.nix
+++ configuration.nix
@@ -52,43 +52,44 @@
     LC_TIME = "en_IN";
   };
 
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
+  hardware = {
+    # Enable OpenGL
+    graphics.enable = true;
+    openrazer.enable = true;
 
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+    nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = true;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = true;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = true;
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      open = true;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
 
   services = {
-    udev.packages = with pkgs; [gnome-settings-daemon];
+    udev.packages = with pkgs; [gnome-settings-daemon openrazer-daemon];
     # Enable the X11 windowing system.
     xserver = {
       enable = true;
@@ -139,11 +140,13 @@
 
   security.rtkit.enable = true;
 
+  users.groups.plugdev = {};
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sid = {
     isNormalUser = true;
     description = "Siddharth Pant";
-    extraGroups = ["networkmanager" "wheel"];
+    extraGroups = ["networkmanager" "wheel" "openrazer" "plugdev"];
     shell = pkgs.zsh;
   };
 
@@ -186,7 +189,7 @@
         zoxide
         bat
         lazygit
-        config.boot.kernelPackages.openrazer
+        openrazer-daemon
         polychromatic
       ]
       ++ (with pkgs.gnomeExtensions; [
