96db6b89922abc7fe14a35cfe2fed1aaa4ced637
SiddharthPant <spsiddharthpant@gmail.com>
2025-05-31 13:43:17 +0530

    Fix locales

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -38,18 +38,23 @@
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = ["en_IN/UTF-8"];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    extraLocaleSettings = {
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF8";
+      LC_ADDRESS = "en_IN";
+      LC_IDENTIFICATION = "en_IN";
+      LC_MEASUREMENT = "en_IN";
+      LC_MONETARY = "en_IN";
+      LC_NAME = "en_IN";
+      LC_NUMERIC = "en_IN";
+      LC_PAPER = "en_IN";
+      LC_TELEPHONE = "en_IN";
+      LC_TIME = "en_IN";
+    };
   };
 
   virtualisation.docker = {
